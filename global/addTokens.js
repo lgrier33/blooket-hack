edfe49db23e59df4ef0f4f22d85d0757b01f74b9
lgrier33 <98117104+lgrier33@users.noreply.github.com>
2022-01-20 16:13:37 -0600

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -15,9 +15,8 @@ async function getName() {
 async function addCurrencies() {
     const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
-    };
+    if (tokens > 50000) {
+        
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
         method: "PUT",
